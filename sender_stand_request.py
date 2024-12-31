9858253df3baafa6158c531d84514eebd60e943f
Danisam207 <brionesdaniel181@gmail.com>
2024-12-30 19:30:41 -0600

    sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,1 +1,9 @@
+import configuration
+import requests
+import data
 
+def post_new_user(user_body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=user_body,
+                         headers=data.headers,
+                         )
