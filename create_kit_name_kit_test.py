349bbb4d71b0b3e2f46b50dfbbc49ac9b3b4ca99
Danisam207 <brionesdaniel181@gmail.com>
2024-12-31 11:43:14 -0600

    create_kit_name_kit_test.py

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -23,31 +23,31 @@ def negative_assert(kit_name):
 
 #Prueba 1. El número permitido de caracteres (1):
 def test_create_kit_1_letter_in_name_get_success_response():
-    positive_assert(data.kit_body_name_t1)
+    positive_assert(data.kit_body_name_P1)
 
 #Prueba 2. El número permitido de caracteres (511):
 def test_create_kit_511_letter_in_name_get_success_response():
-    positive_assert(data.kit_body_name_t2)
+    positive_assert(data.kit_body_name_P2)
 
 #Prueba 3. El número de caracteres es menor que la cantidad permitida (0):
 def test_create_kit_empty_name_get_error_response():
-    negative_assert(data.kit_body_name_t3)
+    negative_assert(data.kit_body_name_P3)
 
 #Prueba 4. El número de caracteres es mayor que la cantidad permitida (512):
 def test_create_kit_512_letter_in_name_get_error_response():
-    negative_assert(data.kit_body_name_t4)
+    negative_assert(data.kit_body_name_P4)
 
 #Prueba 5. Se permiten caracteres especiales:
 def test_create_kit_has_special_symbol_in_name_get_success_response():
-    positive_assert(data.kit_body_name_t5)
+    positive_assert(data.kit_body_name_P5)
 
 #Prueba 6. Se permiten espacios:
 def test_create_kit_has_space_in_name_get_success_response():
-    positive_assert(data.kit_body_name_t6)
+    positive_assert(data.kit_body_name_P6)
 
 #Prueba 7. Se permiten números:
 def test_create_kit_has_number_in_name_get_success_response():
-    positive_assert(data.kit_body_name_t7)
+    positive_assert(data.kit_body_name_P7)
 
 #Prueba 8. El parámetro no se pasa en la solicitud:
 def test_create_kit_no_name_get_error_response():
@@ -57,7 +57,7 @@ def test_create_kit_no_name_get_error_response():
 
 #Prueba 9. Se ha pasado un tipo de parámetro diferente (número):
 def test_create_kit_number_type_name_get_error_response():
-    kit_body = get_kit_body(data.kit_body_name_t9)
+    kit_body = get_kit_body(data.kit_body_name_P9)
     response = sender_stand_request.post_new_client_kit(kit_body)
 
     assert response.status_code == 400
