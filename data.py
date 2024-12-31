82209dee4a7b3875473d8a12798a0792849d58e0
Danisam207 <brionesdaniel181@gmail.com>
2024-12-30 19:27:43 -0600

    data.py

--- data.py
+++ data.py
@@ -1,1 +1,9 @@
+headers = {
+    "Content-Type": "application/json"
+}
 
+user_body = {
+    "firstName": "Armando",
+    "phone": "+856214736978",
+    "address": "8042 Lancaster Ave.Hamburg, NY"
+}
