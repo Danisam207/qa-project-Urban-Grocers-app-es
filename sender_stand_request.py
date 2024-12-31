cfbd064bbe9b98bec1d831e8d7cc45ef6d7d49d1
Danisam207 <brionesdaniel181@gmail.com>
2024-12-30 19:40:04 -0600

    sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -7,3 +7,16 @@ def post_new_user(user_body):
                          json=user_body,
                          headers=data.headers,
                          )
+
+
+def post_new_client_kit(kit_body):
+    create_user = post_new_user(data.user_body)
+    auth_token = create_user.json()['authToken']
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {auth_token}"
+    }
+    return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH,
+                         json=kit_body,
+                         headers=headers
+                         )
